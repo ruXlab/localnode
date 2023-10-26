9ff24d6a0dc95aa22301610458840538d77449f4
ruX <git@rux.vc>
2023-10-26 07:27:34 +0100

    Attempt to build lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,20 @@
 plugins {
     kotlin("jvm") version "1.9.0" apply false
     application
-    `maven-publish`
+    id("maven-publish")
 }
 
 group = "vc.rux.pokefork"
 version = "0.0-SNAPSHOT"
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "vc.rux.pokefork"
+            artifactId = "pokefork"
+
+            from(components["java"])
+        }
+    }
+}
